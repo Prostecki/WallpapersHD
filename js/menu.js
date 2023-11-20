76bc85e051f1c7563ac3434144a49309eebe1271
Mark Taratynov <mark.taratynov@gmail.com>
2023-11-20 15:46:12 +0100

    menu button

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,19 @@
 
 export function toggleMenu(){
-    if (menuBar.style.top === "-200px" || menuBar.style.top === '') {
-        menuBar.style.top = '0px';
-
+    if (menuBar.style.left === "-500px" || menuBar.style.left === '') {
+        menuBar.style.left = '50px';
     } else {
-        menuBar.style.top = '-200px';
+        menuBar.style.left = '-500px';
     }
     console.log('menu toggled!');
-}
+};
 
 export function menuBackground() {
     const menuHeader = document.querySelector('.nav-container');
     if (window.scrollY >= 10) {
-        // menuHeader.style.backgroundColor = 'grey';
+        menuHeader.style.backgroundColor = 'grey';
         menuHeader.style.transition = '0.5s';
     } else {
         menuHeader.style.backgroundColor = 'transparent';
     }
-}
+};
